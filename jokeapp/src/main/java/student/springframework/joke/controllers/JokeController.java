de83e8f5cbeb3d4b4b7ad5a380f81c71eaed4a3f
snair <snair@lendingclub.com>
2019-08-15 11:28:40 -0700

    fixes for 400 error issue

--- jokeapp/src/main/java/student/springframework/joke/controllers/JokeController.java
+++ jokeapp/src/main/java/student/springframework/joke/controllers/JokeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import student.springframework.joke.jokeapp.JokeService;
+import student.springframework.joke.services.JokeService;
 
 @Controller
 public class JokeController {
